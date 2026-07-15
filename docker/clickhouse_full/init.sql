b5ea3b84e71fede74eb639af10ae24efbfe17bdf
kregis <olya.ivenkova@gmail.com>
2026-07-15 18:51:04 +0300

    Fix bug

--- docker/clickhouse_full/init.sql
+++ docker/clickhouse_full/init.sql
@@ -16,7 +16,7 @@ SELECT
     first_name,
     middle_name,
     concat(last_name, ' ', first_name, ' ', middle_name) as full_name
-FROM file('/data/people.csv', 'CSV');
+FROM file('people.csv', 'CSV', 'id UInt32, last_name String, first_name String, middle_name String');
 
 ALTER TABLE people
 ADD INDEX idx_token full_name TYPE tokenbf_v1(32768, 3, 0) GRANULARITY 1;
--- docker/clickhouse_fuzzy/init.sql
+++ docker/clickhouse_fuzzy/init.sql
@@ -16,7 +16,7 @@ SELECT
     first_name,
     middle_name,
     concat(last_name, ' ', first_name, ' ', middle_name) as full_name
-FROM file('/data/people.csv', 'CSV');
+FROM file('people.csv', 'CSV', 'id UInt32, last_name String, first_name String, middle_name String');
 
 ALTER TABLE people
 ADD INDEX idx_ngram full_name TYPE ngrambf_v1(3, 4096, 3, 0) GRANULARITY 1;

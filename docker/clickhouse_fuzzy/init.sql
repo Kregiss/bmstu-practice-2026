be4b1b56ff11a0646a210596eabcabfa6ddf0399
Kregiss <145288385+Kregiss@users.noreply.github.com>
2026-07-16 16:50:59 +0300

    Revert "Dev1"

--- docker/clickhouse_fuzzy/init.sql
+++ docker/clickhouse_fuzzy/init.sql
@@ -19,6 +19,4 @@ SELECT
 FROM file('/data/people.csv', 'CSV');
 
 ALTER TABLE people
-ADD INDEX idx_ngram full_name TYPE ngrambf_v1(3, 32768, 3, 0) GRANULARITY 1;
-
-ALTER TABLE people MATERIALIZE INDEX idx_ngram;
+ADD INDEX idx_ngram full_name TYPE ngrambf_v1(3, 4096, 3, 0) GRANULARITY 1;
